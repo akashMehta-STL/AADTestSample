36aaf5c8f482dbef15e695234b7ffe55f734f79f
Aakash Mehta <akash.mehta@solutelabs.com>
2018-07-14 19:28:41 +0530

    Mocking object and pre define value using when.

--- app/src/main/java/stllpt/com/aadtestsample/MainActivity.kt
+++ app/src/main/java/stllpt/com/aadtestsample/MainActivity.kt
@@ -1,12 +1,27 @@
 package stllpt.com.aadtestsample
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
+open class MainActivity : AppCompatActivity() {
+    lateinit var etEmail: EditText
+    lateinit var etPassword : EditText
+    lateinit var btnSubmit: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initUI()
     }
+
+    open fun initUI() {
+        etEmail = findViewById(R.id.etUserName)
+        etPassword = findViewById(R.id.etPassword)
+        btnSubmit = findViewById(R.id.btnSubmit)
+    }
+
+    fun appString(context: Context) = context.getString(R.string.app_name)
 }
--- app/src/test/java/stllpt/com/aadtestsample/ExampleUnitTest.kt
+++ app/src/test/java/stllpt/com/aadtestsample/ExampleUnitTest.kt
@@ -1,17 +1,51 @@
 package stllpt.com.aadtestsample
 
+import android.content.Context
+import android.util.Patterns
 import org.junit.Test
+
+import org.mockito.Mockito.*
+
 import org.hamcrest.Matchers.*
 import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import java.util.regex.Pattern
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@RunWith(MockitoJUnitRunner::class)
 class ExampleUnitTest {
+    @Mock
+    lateinit var context: Context
+
+    val EMAIL_ADDRESS_PATTERN = Pattern.compile(
+            "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                    "\\@" +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                    "(" +
+                    "\\." +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                    ")+"
+    )
+
     @Test
     fun addition_isCorrect() {
-        assertThat(4 == (2+2), `is`(true))
+        assertThat(EMAIL_ADDRESS_PATTERN.matcher("a@g.c").matches(), `is`(true))
+    }
+
+    /**
+     * when method will be used to predefine value for a method call.
+     */
+    @Test
+    fun readStringFromContext() {
+        `when`(context.getString(R.string.app_name)).thenReturn("AADTestSample")
+        val mainActivity = MainActivity()
+        val result = mainActivity.appString(context)
+        assertThat(result, `is`("AADTestSample"))
     }
 }
