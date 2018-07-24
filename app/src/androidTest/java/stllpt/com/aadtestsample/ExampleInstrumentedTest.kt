53f857b2e83ca6d527942b0234eeab626d252464
Aakash Mehta <akash.mehta@solutelabs.com>
2018-07-24 15:11:18 +0530

    Instrumental test 2 : Test on activity result pending.

--- app/src/androidTest/java/stllpt/com/aadtestsample/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/stllpt/com/aadtestsample/ExampleInstrumentedTest.kt
@@ -1,15 +1,22 @@
 package stllpt.com.aadtestsample
 
+import android.app.Activity
+import android.app.Instrumentation
 import android.content.Intent
+import android.provider.MediaStore
 import android.support.test.InstrumentationRegistry
 import android.support.test.espresso.Espresso.*
 import android.support.test.espresso.IdlingRegistry
 import android.support.test.espresso.action.ViewActions.*
 import android.support.test.espresso.assertion.ViewAssertions.*
+import android.support.test.espresso.intent.Intents
+import android.support.test.espresso.intent.matcher.IntentMatchers
+import android.support.test.espresso.intent.rule.IntentsTestRule
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import org.hamcrest.Matchers
+import org.hamcrest.core.IsNot
 import org.junit.After
 
 import org.junit.Test
@@ -39,17 +46,8 @@ class ExampleInstrumentedTest {
         assertEquals("stllpt.com.aadtestsample", appContext.packageName)
     }
 
-    /**
-     * @exception ValidationError The @Rule 'mMainActivityTestActivity' must be public.
-     * on using simply @Rule
-     * ref :
-     * https://stackoverflow.com/questions/29945087/kotlin-and-new-activitytestrule-the-rule-must-be-public
-     */
     @get:Rule
-    public var mMainActivityTestActivity = ActivityTestRule(MainActivity::class.java)
-
-    @get:Rule
-    public var mSecondaryActivityTestActivity = ActivityTestRule(SecondaryActivity::class.java, true, false)
+    public val mSecondaryTestIntent: IntentsTestRule<SecondaryActivity> = IntentsTestRule<SecondaryActivity>(SecondaryActivity::class.java)
 
     @Test
     fun checkSignIn() {
@@ -60,17 +58,46 @@ class ExampleInstrumentedTest {
     }
 
     @Before
-    fun initWithStubbedData() {
-        val intent = Intent()
-        intent.putExtra(SecondaryActivity.USER_NAME, userName)
-        intent.putExtra(SecondaryActivity.PASSWORD, password)
-        mSecondaryActivityTestActivity.launchActivity(intent)
-        registerIdlingResources()
+    fun registerIdlingResources() {
+        IdlingRegistry.getInstance().register(mSecondaryTestIntent.activity.getCountingIdlingResource())
     }
+
+    @Test
+    fun addImageNote_showImage() {
+        defineCaptureImageAction()
+
+        onView(withId(R.id.ivProfile)).check(matches(IsNot.not(isDisplayed())))
+
+        performOptionMenuClick()
+
+        onView(withId(R.id.ivProfile)).check(matches(isDisplayed()))
+    }
+
+    private fun performOptionMenuClick() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext())
+        onView(withText(R.string.menu_title_capture_image)).perform(click())
+    }
+
+    private fun defineCaptureImageAction() {
+        val result = getActivityResult()
+        Intents.intending(IntentMatchers.hasAction(MediaStore.ACTION_IMAGE_CAPTURE))
+                .respondWith(result)
+    }
+
+    private fun getActivityResult(): Instrumentation.ActivityResult {
+        return Instrumentation.ActivityResult(Activity.RESULT_OK, null)
+    }
+//    fun initWithStubbedData() {
+//        val intent = Intent()
+//        intent.putExtra(SecondaryActivity.USER_NAME, userName)
+//        intent.putExtra(SecondaryActivity.PASSWORD, password)
+//        mSecondaryActivityTestActivity.launchActivity(intent)
+//        registerIdlingResources()
+//    }
 
     @After
     fun unregisterIdlingResources() {
-        IdlingRegistry.getInstance().unregister(mSecondaryActivityTestActivity.activity.getCountingIdlingResource())
+        IdlingRegistry.getInstance().unregister(mSecondaryTestIntent.activity.getCountingIdlingResource())
     }
 
     @Test
@@ -78,9 +105,5 @@ class ExampleInstrumentedTest {
         onView(withId(R.id.tvUserName)).check(matches(withText(userName)))
         onView(withId(R.id.tvPassword)).check(matches(withText(password)))
         onView(withId(R.id.ivProfile)).check(matches(Matchers.allOf(ImageViewHasDrawableMatcher.hasDrawable(), isDisplayed())))
-    }
-
-    fun registerIdlingResources() {
-        IdlingRegistry.getInstance().register(mSecondaryActivityTestActivity.activity.getCountingIdlingResource())
     }
 }
--- app/src/main/java/stllpt/com/aadtestsample/secondary/SecondaryActivity.kt
+++ app/src/main/java/stllpt/com/aadtestsample/secondary/SecondaryActivity.kt
@@ -1,11 +1,26 @@
 package stllpt.com.aadtestsample.secondary
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.annotation.VisibleForTesting
 import android.support.test.espresso.IdlingResource
+import android.support.v4.content.ContextCompat
+import android.support.v4.util.Preconditions.checkState
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.GlideDrawable
+import com.bumptech.glide.request.animation.GlideAnimation
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget
 import kotlinx.android.synthetic.main.activity_secondary.*
 import stllpt.com.aadtestsample.R
 import stllpt.com.aadtestsample.utils.EspressoIdlingResource
@@ -24,19 +39,65 @@ class SecondaryActivity : AppCompatActivity(), SecondaryPresenter.View {
     companion object {
         const val USER_NAME = "userName"
         const val PASSWORD = "password"
+        const val ACTION_CAPTURE_IMAGE = 2001
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        data?.let {
+            val imageUrl = "file:///android_asset/atsl-logo.png"
+            if (resultCode == Activity.RESULT_OK) {
+                ivProfile.visibility = View.VISIBLE
+
+                // The image is loaded in a different thread so in order to UI-test this, an idling resource
+                // is used to specify when the app is idle.
+                EspressoIdlingResource.increment() // App is busy until further notice.
+
+                // This app uses Glide for image loading
+                Glide.with(this)
+                        .load(imageUrl)
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .centerCrop()
+                        .into(object : GlideDrawableImageViewTarget(ivProfile) {
+                            override fun onResourceReady(resource: GlideDrawable,
+                                                animation: GlideAnimation<in GlideDrawable>) {
+                                super.onResourceReady(resource, animation)
+                                EspressoIdlingResource.decrement() // Set app as idle.
+                            }
+                        })
+                try {
+                    Thread.sleep(5000)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+
+
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_secondary_activity, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            R.id.menuCaptureImage -> {
+                startActivityForResult(Intent(MediaStore.ACTION_IMAGE_CAPTURE),
+                        ACTION_CAPTURE_IMAGE)
+            }
+        }
+        return true
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_secondary)
 
-        username = intent.extras.getString(USER_NAME)
-        password = intent.extras.getString(PASSWORD)
+        username = ""
+        password = ""
         initUI()
-
-        val secondaryModel = SecondaryModel(username, password)
-        mPresenter = SecondaryPresenter(this)
-        mPresenter.showData(secondaryModel)
     }
 
     private fun initUI() {
